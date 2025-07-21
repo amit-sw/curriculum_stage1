91fc0d3ca002096d1756a6f9f08a37ec7d67d2c8
amit-sw <your-email@example.com>
2025-07-20 17:45:30 -0700

    Add env reading

--- config.py
+++ config.py
@@ -1,6 +1,9 @@
 # config.py
 import streamlit as st
+import os
+from dotenv import load_dotenv
+load_dotenv()
 DEFAULT_PROMPT = "Generate a concise curriculum for the following topic."
 TOPIC_PROMPT = "Generate a topic name for the following sentence."
-OPENAI_API_KEY = st.secrets['OPENAI_API_KEY']
+OPENAI_API_KEY = st.secrets.get('OPENAI_API_KEY', os.environ.get('OPENAI_API_KEY'))
 DB_FILE = "output/curriculum.db"
